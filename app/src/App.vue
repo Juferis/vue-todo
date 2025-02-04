97666ce2d275411c4ad34e9e1bfa2fc429126816
Juferis <66511872+Juferis@users.noreply.github.com>
2025-02-04 16:37:43 +0900

    CSS 작성 및 다크모드 추가

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import './assets/style.css';
 import TodoList from './components/todo-list/TodoList.vue';
+import { LABELS_TODO } from './contants';
+
+const isDarkMode = ref(false);
+
+const toggleDarkMode = () => {
+  isDarkMode.value = !isDarkMode.value;
+  document.body.classList.toggle('dark-mode', isDarkMode.value);
+  localStorage.setItem('darkMode', JSON.stringify(isDarkMode.value));
+};
 </script>
 
 <template>
-  <TodoList />
+  <div class="container">
+    <header>
+      <h2>{{ LABELS_TODO.TITLE }}</h2>
+      <button class="dark-mode-toggle" @click="toggleDarkMode">
+        {{ isDarkMode ? '🌙' : '☀️' }}
+      </button>
+    </header>
+    <TodoList />
+  </div>
 </template>
-
-<style scoped></style>
