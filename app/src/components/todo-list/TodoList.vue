e383736700a03d36110fef7c27829d746442c0f8
Juferis <66511872+Juferis@users.noreply.github.com>
2025-02-04 15:32:47 +0900

    Todo list vue template 생성 및 import

---
+++ app/src/components/todo-list/TodoList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Todo } from './types';
+
+const todos = ref<Todo[]>([]);
+</script>
+
+<template>
+  <div>
+    <h2>Todo List</h2>
+    <div>
+      <input placeholder="할 일을 입력하세요" />
+      <button>추가</button>
+    </div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}
+        <button>삭제</button>
+      </li>
+    </ul>
+  </div>
+</template>
