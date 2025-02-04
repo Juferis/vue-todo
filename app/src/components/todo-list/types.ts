f3bc28b84f59bb830b89f3086be298c7345b9a8f
Juferis <66511872+Juferis@users.noreply.github.com>
2025-02-04 15:31:45 +0900

    todo list 인터페이스 및 enum 생성

---
+++ app/src/components/todo-list/types.ts
@@ -0,0 +1,13 @@
+export enum TodoStatus {
+    COMPLETE = 1,
+    INCOMPLETE = 0
+}
+
+export interface Todo {
+    id: number;
+    text: string;
+    status: TodoStatus;
+    assignee: string;
+    deadline: string;
+    createdAt: string;
+}
